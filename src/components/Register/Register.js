c6c1b926794c48d03512dc3bd2187c8d3fd082e0
mositodrone <mositodrone@gmail.com>
2020-05-02 23:23:41 +0100

    ok

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/register', {
+    fetch('https://hidden-wave-38166.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
